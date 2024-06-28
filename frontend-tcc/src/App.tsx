b4065c51066ce6ce96a59522bce0105ba387c612
leonardo-fontes <leofonttes@gmail.com>
2024-06-28 14:33:03 -0300

    starting

---
+++ frontend-tcc/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-400">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
