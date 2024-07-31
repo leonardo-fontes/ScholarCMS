c165cf1a484d28a8ec1302a0d518741bb76819b6
Leonardo Fontes <leonardo.fontes@totvs.com.br>
2024-07-31 01:25:18 -0300

    Adicionando funcionalidades a landing page

--- frontend-tcc/src/components/partials/Invite.tsx
+++ frontend-tcc/src/components/partials/Invite.tsx
@@ -1,26 +1,50 @@
 interface Invite {
-  title: string;
-  text: string;
-  sourceImage: string;
-  titleButton: string;
+    title: string;
+    text: string;
+    sourceImage: string;
+    titleButton: string;
+    rightImage?: boolean;
 }
 import Button from "../inputs/Button";
-export default function Invite({ title, text, sourceImage, titleButton } : Invite) {
-  return (
-    <>
-      <img
-        className="bg-gray-300 mt-20 rounded-lg mx-7"
-        src={sourceImage}
-        alt=""
-      />
-      <div className="flex flex-col px-7 py-8 gap-6 ">
-        <h1 className="text-2xl font-semibold">{title}</h1>
-        <p className="text-sm">{text}</p>
-      </div>
-      <Button
-        text={titleButton}
-        classname="text-lg font-semibold text-white bg-gray-500 mx-7"
-      />
-    </>
-  );
+export default function Invite({
+    title,
+    text,
+    sourceImage,
+    titleButton,
+    rightImage = false,
+}: Invite) {
+    return (
+        <div className="flex flex-col md:flex-row md:items-center">
+            <img
+                className={
+                    rightImage
+                        ? `md:hidden`
+                        : `bg-gray-300 mt-20 rounded-lg mx-7 md:w-[50%]`
+                }
+                src={sourceImage}
+                alt=""
+            />
+            <div className="flex flex-col md:gap-8">
+                <div className="flex md:flex flex-col px-7 py-8 gap-6">
+                    <h1 className="text-2xl md:text-4xl font-semibold">
+                        {title}
+                    </h1>
+                    <p className="text-sm md:text-lg">{text}</p>
+                </div>
+                <Button
+                    text={titleButton}
+                    classname="text-lg md:text-xl md:px-40 md:py-2 font-semibold md:self-center text-white bg-gray-500 mx-7"
+                />
+            </div>
+            <img
+                className={
+                    rightImage
+                        ? `bg-gray-300 mt-20 rounded-lg mx-7 md:w-[50%] md:block hidden`
+                        : `hidden`
+                }
+                src={sourceImage}
+                alt=""
+            />
+        </div>
+    );
 }
--- frontend-tcc/src/pages/LandingPage.tsx
+++ frontend-tcc/src/pages/LandingPage.tsx
@@ -1,93 +1,122 @@
 import Button from "../components/inputs/Button";
 import Icon from "../components/icons/index";
 import Invite from "../components/partials/Invite";
+import { useEffect, useState } from "react";
+
+interface Users {
+    id: number;
+    text: string;
+    img: string;
+    name: string;
+}
 
 export default function LandingPage() {
-  const userText = [
-    "socorro",
-    "bom dia",
-    "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.",
-  ];
+    const users: Users[] = [
+        {
+            id: 1,
+            name: "Lukako",
+            text: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.",
+            img: "/jovens.png",
+        },
+        {
+            id: 2,
+            name: "Alisshow",
+            text: "bom diabom diabom diabom diabom diabom diabom diabom diabom diabom diabom diabom diabom diabom diabom diabom diabom diabom diabom dia",
+            img: "/jovens.png",
+        },
+    ];
 
-  return (
-    <div className="w-full flex flex-col text-white bg-gray-300">
-      <div className="bg-gray-700 flex justify-between px-7 items-center py-4 sticky top-0">
-        <Icon className="hover:cursor-pointer" name="logo" size={60} />
-        <Button
-          text="Login"
-          classname="text-lg text-white bg-gray-500 px-6 font-semibold"
-        />
-      </div>
-      <Invite
-        text="Lorem Ipsum é simplesmente uma simulação de texto da indústria
+    const [teste, setTeste] = useState(0);
+
+    useEffect(() => {}, [teste]);
+    return (
+        <div className="w-full flex flex-col text-white bg-gray-300 ">
+            <div className="bg-gray-700 flex justify-between px-7 items-center py-4 sticky top-0">
+                <Icon className="hover:cursor-pointer" name="logo" size={60} />
+                <Button
+                    text="Login"
+                    classname="text-lg text-white bg-gray-500 px-6 font-semibold"
+                />
+            </div>
+            <Invite
+                text="Lorem Ipsum é simplesmente uma simulação de texto da indústria
           tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
           quando um impressor desconhecido pegou uma bandeja de tipos e os
           embaralhou para fazer um livro de modelos de tipos."
-        title="SEJA UM DOADOR!"
-        titleButton="FAÇA PARTE DO NOSSO TIME"
-        sourceImage="/jovens.png"
-      />
-      <Invite
-        text="Lorem Ipsum é simplesmente uma simulação de texto da indústria
+                title="SEJA UM DOADOR!"
+                titleButton="FAÇA PARTE DO NOSSO TIME"
+                sourceImage="/jovens.png"
+                rightImage={true}
+            />
+            <Invite
+                text="Lorem Ipsum é simplesmente uma simulação de texto da indústria
           tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
           quando um impressor desconhecido pegou uma bandeja de tipos e os
           embaralhou para fazer um livro de modelos de tipos."
-        title="APOIE PESSOAS DA SUA COMUNIDADE!"
-        titleButton="FAÇA PARTE DO NOSSO TIME"
-        sourceImage="/jovens.png"
-      />
-      <div className="flex flex-col justify-center mx-7 items-center mt-20 ">
-        <h2 className="font-semibold text-2xl mb-12">
-          CONHEÇA BENEFICIÁRIOS PRÓXIMOS A VOCÊ
-        </h2>
-        <img
-          className="rounded-full aspect-square w-32 bg-red-400 mb-2"
-          src="/jovens.png"
-          alt=""
-        />
-        <p className="text-lg font-semibold">Larissa Mamoela</p>
-        <div className="flex justify-around w-full my-8">
-          <img
-            className="rounded-full aspect-square w-12 bg-red-400 mb-2"
-            src="/jovens.png"
-            alt=""
-          />
-          <img
-            className="rounded-full aspect-square w-12 bg-red-400 mb-2"
-            src="/jovens.png"
-            alt=""
-          />
-          <img
-            className="rounded-full aspect-square w-12 bg-red-400 mb-2"
-            src="/jovens.png"
-            alt=""
-          />
+                title="APOIE PESSOAS DA SUA COMUNIDADE!"
+                titleButton="FAÇA PARTE DO NOSSO TIME"
+                sourceImage="/jovens.png"
+            />
+            <div className="flex flex-col justify-center mx-7 items-center mt-20 ">
+                <h2 className="font-semibold md:text-4xl text-2xl mb-10 text-center">
+                    CONHEÇA BENEFICIÁRIOS PRÓXIMOS A VOCÊ
+                </h2>
+                <img
+                    className="rounded-full aspect-square w-32 md:w-44 bg-red-400 mb-2"
+                    src="/jovens.png"
+                    alt=""
+                />
+                <p className="text-lg md:text-2xl font-semibold md:mb-4">
+                    {users[teste].name}
+                </p>
+                <div className="flex justify-between md:justify-around w-full my-8">
+                    <img
+                        onClick={() => {
+                            setTeste(0);
+                        }}
+                        className="rounded-full aspect-square w-16 md:w-24 bg-red-400 mb-2"
+                        src="/jovens.png"
+                        alt=""
+                    />
+                    <img
+                        className="rounded-full aspect-square w-16 md:w-24 bg-green-400 mb-2"
+                        src="/jovens.png"
+                        alt=""
+                    />
+                    <img
+                        className="rounded-full aspect-square w-16 md:w-24 bg-yellow-400 mb-2"
+                        src="/jovens.png"
+                        alt=""
+                    />
+                </div>
+                <p className="bg-white rounded-lg min-h-40 min-w-80 md:w-[60%] text-black px-4 p-2 text-wrap">
+                    {users[teste].text}
+                </p>
+                <div className="flex justify-between md:justify-around w-full my-8">
+                    <img
+                        onClick={() => {
+                            setTeste(1);
+                        }}
+                        className="rounded-full aspect-square w-16 md:w-24 bg-blue-400 mb-2"
+                        src="/jovens.png"
+                        alt=""
+                    />
+                    <img
+                        className="rounded-full aspect-square w-16 md:w-24 bg-red-400 mb-2"
+                        src="/jovens.png"
+                        alt=""
+                    />
+                    <img
+                        className="rounded-full aspect-square w-16 md:w-24 bg-orange-400 mb-2"
+                        src="/jovens.png"
+                        alt=""
+                    />
+                </div>
+                <Button
+                    text={"FAÇA PARTE DO NOSSO TIME"}
+                    classname="text-lg md:text-2xl font-semibold text-white bg-gray-500 w-full md:w-[45%] md:py-2"
+                />
+            </div>
         </div>
-        <p className="bg-white rounded-lg min-h-40 min-w-80 text-black px-4 p-2 text-wrap">
-          {userText[2]}
-        </p>
-        <div className="flex justify-around w-full my-8">
-          <img
-            className="rounded-full aspect-square w-12 bg-red-400 mb-2"
-            src="/jovens.png"
-            alt=""
-          />
-          <img
-            className="rounded-full aspect-square w-12 bg-red-400 mb-2"
-            src="/jovens.png"
-            alt=""
-          />
-          <img
-            className="rounded-full aspect-square w-12 bg-red-400 mb-2"
-            src="/jovens.png"
-            alt=""
-          />
-        </div>
-        <Button
-        text={"FAÇA PARTE DO NOSSO TIME"}
-        classname="text-lg font-semibold text-white bg-gray-500 w-full"
-      />
-      </div>
-    </div>
-  );
+    );
 }
