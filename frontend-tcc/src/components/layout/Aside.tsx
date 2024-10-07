f1097bcdebe862e7695227443f5695c4a6c856b8
Leonardo Fontes <leonardo.fontes@totvs.com.br>
2024-10-07 19:40:57 -0300

    aside

---
+++ frontend-tcc/src/components/layout/Aside.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Aside() {
+    return (
+        <aside className="absolute left-0 top-[40%] w-[320px] flex flex-col">
+            Aside
+        </aside>
+    );
+}
--- frontend-tcc/src/pages/Platform/index.tsx
+++ frontend-tcc/src/pages/Platform/index.tsx
@@ -1,4 +1,4 @@
-
+import Aside from "../../components/layout/Aside";
 import Publication from "../../components/partials/Publication";
 import { usePlatform } from "./usePlatform";
 
@@ -6,13 +6,12 @@ export default function PlatformPage() {
     const { publications } = usePlatform();
     return (
         <div className="w-full flex flex-col bg-white py-1 items-center container mx-auto">
+            <Aside />
             <section className="w-[40%] m-20">
                 {publications.map((publication) => (
                     <Publication key={publication.post.id} {...publication} />
                 ))}
             </section>
         </div>
-
-
     );
 }
