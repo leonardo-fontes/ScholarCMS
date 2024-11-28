4c28d46a821092cd31ae943652731f050102448a
alisson <alisson.vieira@totvs.com.br>
2024-11-28 13:10:32 -0300

    aligning

--- frontend-tcc/src/components/layout/Navbar.tsx
+++ frontend-tcc/src/components/layout/Navbar.tsx
@@ -73,52 +73,53 @@ export default function Navbar() {
           </div>
         )}
 
-        {user && (
-          <div className="relative">
-            <button
-              onClick={() => setShowNotifications(true)}
-              className="p-2 hover:bg-gray-700 rounded-full relative mx-4"
-            >
-              <div className="w-6 h-6 flex items-center justify-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6 text-white"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                  />
-                </svg>
+        <div className="flex gap-4 items-center">
+          {user && (
+            <div className="relative">
+              <button
+                onClick={() => setShowNotifications(true)}
+                className="p-2 hover:bg-gray-700 rounded-full relative mx-4"
+              >
+                <div className="w-6 h-6 flex items-center justify-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-white"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                    />
+                  </svg>
 
-                <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
-                  {unreadCount}
-                </span>
-                {isLoading && (
-                  <div className="absolute -top-1 -right-1 h-2 w-2 bg-blue-500 rounded-full animate-pulse" />
-                )}
-              </div>
-            </button>
+                  <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+                    {unreadCount}
+                  </span>
+                  {isLoading && (
+                    <div className="absolute -top-1 -right-1 h-2 w-2 bg-blue-500 rounded-full animate-pulse" />
+                  )}
+                </div>
+              </button>
 
-            <NotificationModal
-              isOpen={showNotifications}
-              onClose={() => setShowNotifications(false)}
-              notifications={notifications}
-            />
-          </div>
-        )}
-
-        <Button
-          href="/login"
-          classname="text-lg md:text-xl text-primary bg-white px-6 md:px-8 py-1 md:py-2 font-semibold"
-          onClick={user ? signout : undefined}
-        >
-          {user ? "Sair" : "Entrar"}
-        </Button>
+              <NotificationModal
+                isOpen={showNotifications}
+                onClose={() => setShowNotifications(false)}
+                notifications={notifications}
+              />
+            </div>
+          )}
+          <Button
+            href="/login"
+            classname="text-lg md:text-xl text-primary bg-white px-6 md:px-8 py-1 md:py-2 font-semibold"
+            onClick={user ? signout : undefined}
+          >
+            {user ? "Sair" : "Entrar"}
+          </Button>
+        </div>
       </div>
     )
   );
